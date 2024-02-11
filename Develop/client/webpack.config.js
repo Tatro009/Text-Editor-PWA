8d3525fb8de3ca5f44721be9761588a97ae7f300
Rob Tatro <tatro009@gmail.com>
2024-02-11 15:02:59 -0600

    Updated index.html

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,30 +1,87 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/js/index.js',
+    install: './src/js/install.js'
+  },
 
-    module: {
-      rules: [
-        
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      // Handle JavaScript files with Babel
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      
+      // Handle CSS files
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    // Clean the 'dist' folder before each build
+    new CleanWebpackPlugin(),
+    
+    // Generate HTML files
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      filename: 'index.html',
+      chunks: ['main'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      filename: 'install.html',
+      chunks: ['install'],
+    }),
+    
+    // Add and configure Workbox plugins
+    new InjectManifest({
+      swSrc: './client/src-sw.js', // Service worker source file
+      swDest: 'service-worker.js', // Service worker destination file
+    }),
+
+    new WebpackPwaManifest({
+      name: 'Just Another Text Editor',
+      short_name: 'J.A.T.E',
+      description: 'A simple text editor for writing notes and code snippets',
+      background_color: '#ffffff',
+      theme_color: '#2196f3',
+      icons: [
+        {
+          src: path.resolve('client/src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes for different devices
+          destination: path.join('assets', 'icons'),
+          ios: true,
+        },
       ],
-    },
-  };
+      filename: 'manifest.json', // Name of the output manifest file
+      crossorigin: null, // Setting crossorigin to null will disable CORS headers
+    }),
+    // Generate a manifest file to map output file names to their corresponding input file
+    new WebpackManifestPlugin()
+  ],
 };
